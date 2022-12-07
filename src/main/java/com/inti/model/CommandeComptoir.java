b7e936b856759560d465426b86627385f85a44be
jonathan <jonathan@BKE592H>
2022-12-07 10:36:57 +0100

    creation model commande

---
+++ src/main/java/com/inti/model/CommandeComptoir.java
@@ -0,0 +1,28 @@
+package com.inti.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "CompteProjet")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommandeComptoir {
+
+	@Id
+	private long idCommandeComptoir;
+	private double prixCommande;
+	private boolean aPayer;
+
+//	@OneToOne(cascade = CascadeType.ALL)
+//	@JoinColumn(name = "idCommandePizza")
+//	private CommandePizza commandePizza ;
+
+}
