b344f616cbd4595a572b3a04ff5ecf6688ab2157
theolorenzi <theo.lorenzi@laposte.net>
2022-12-07 09:53:10 +0100

    Classe Pizza ajoutée

---
+++ src/main/java/com/inti/model/Pizza.java
@@ -0,0 +1,28 @@
+package com.inti.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pizza {
+	
+	@Id
+	private int id;
+	private String nom;
+	private String taille;
+	private double prix;
+	private String ingredients;
+	private String typePate;
+	private boolean livraison;
+	private String heureLivraison;
+
+}
