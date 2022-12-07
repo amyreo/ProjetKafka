48d46357ca32d9812f7861597dc857ae76100b06
theolorenzi <theo.lorenzi@laposte.net>
2022-12-07 15:15:37 +0100

    wesh

--- src/main/java/com/inti/controller/CommandePizzaController.java
+++ src/main/java/com/inti/controller/CommandePizzaController.java
@@ -18,7 +18,6 @@ import com.inti.model.CommandePizza;
 import com.inti.model.Pizza;
 import com.inti.repository.IPizzaRepository;
 
-
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -26,72 +25,71 @@ import lombok.extern.slf4j.Slf4j;
 public class CommandePizzaController {
 
 	Scanner scan = new Scanner(System.in);
-	int numCommande=0;
-	
+	int numCommande = 0;
+
 	@Autowired
 	IPizzaRepository ipr;
-	
+
 	@Autowired
 	KafkaTemplate<Object, CommandePizza> kte;
 
-	
 	public void creationCommande() {
-		
-	ArrayList<Pizza> listeCommande = new ArrayList<>();
-		
-	double prix=0;
-	
-	System.out.println("Quelle pizza voulez vous ? Rentrer son identifiant.");
-	int idPizza = scan.nextInt();
-	do {
-	Pizza pizzaClient = ipr.getReferenceById(idPizza);
-	listeCommande.add(idPizza, pizzaClient);
-	prix += ipr.prixPizza(idPizza);
-	System.out.println("Si vous voulez une autre pizza rentrer son identifiant même si vous en voulez une du même type ou si vous avez terminé rentrer 777");
-	idPizza = scan.nextInt();
-	}while(idPizza != 777);
-	
-	CommandeComptoir commandeComptoir = new CommandeComptoir();
-		System.out.println("voulez vous payer sur place ou a emporté ? P/E" );	
-		String payer =scan.next();
-		System.out.println("votre nom ?" );	
-		String nom =scan.next();
+
+		ArrayList<Pizza> listeCommande = new ArrayList<>();
+
+		double prix = 0;
+
+		System.out.println("Quelle pizza voulez vous ? Rentrer son identifiant.");
+		int idPizza = scan.nextInt();
+		do {
+			Pizza pizzaClient = ipr.getReferenceById(idPizza);
+			listeCommande.add(idPizza, pizzaClient);
+			prix += ipr.prixPizza(idPizza);
+			System.out.println(
+					"Si vous voulez une autre pizza rentrer son identifiant même si vous en voulez une du même type ou si vous avez terminé rentrer 777");
+			idPizza = scan.nextInt();
+		} while (idPizza != 777);
+
+		CommandeComptoir commandeComptoir = new CommandeComptoir();
+		System.out.println("voulez vous payer sur place ou a emporté ? P/E");
+		String payer = scan.next();
+		System.out.println("votre nom ?");
+		String nom = scan.next();
 		if (payer == "P") {
-		commandeComptoir.setAPayer(false);
-		}else {
-		commandeComptoir.setAPayer(true);
+			commandeComptoir.setAPayer(false);
+		} else {
+			commandeComptoir.setAPayer(true);
 		}
 		commandeComptoir.setPrixCommande(prix);
 		commandeComptoir.setIdCommandeComptoir(numCommande);
 		commandeComptoir.setNomClient(nom);
-	CommandePizza commandeClient = new CommandePizza(numCommande, prix, listeCommande, commandeComptoir);
-	System.out.println(commandeClient.toString());
-	numCommande++;
+		CommandePizza commandeClient = new CommandePizza(numCommande, prix, listeCommande, commandeComptoir);
+		System.out.println(commandeClient.toString());
+		numCommande++;
 	}
-	
+
 	@GetMapping("/sendPizza")
-	public void SendPizza(CommandePizza commandePizza)
-	{
+	public void SendPizza(CommandePizza commandePizza) {
 		log.info("l'objet " + commandePizza + "a bien été envoyé");
-	
-		Message <CommandePizza> mess =MessageBuilder
-		 .withPayload(commandePizza)
-		 .setHeader(KafkaHeaders.TOPIC, "topicComptoir}").build(); 
+
+		Message<CommandePizza> mess = MessageBuilder.withPayload(commandePizza)
+				.setHeader(KafkaHeaders.TOPIC, "topicComptoir").build();
 		kte.send(mess);
 	}
-	
 
 	@GetMapping("/comptoir")
 	public void prixAPayer() {
-		
+
 //		System.out.println("Le montant des pizzas du client s'élève à " + prix + " €");
 	}
 
 	@GetMapping("/cuisine")
 	public void pizzaAFaire() {
 
-		// récupérer la liste de pizza à faire avec leurs quantités(for each pizza add to the list)
-		// for each get quantité et faire le total de pizza à faire grâce à la variable quantité.
+		// récupérer la liste de pizza à faire avec leurs quantités(for each pizza add
+		// to the list)
+		// for each get quantité et faire le total de pizza à faire grâce à la variable
+		// quantité.
 
 //		System.out.println("Les pizzas à faire sont les suivantes : "+listePizzas);
 	}
